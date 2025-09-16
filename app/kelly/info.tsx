baabef1e3099b366c63c5a04f09da9c04ca405f3
James Kelly Espera <esperajameskelly123@gmail.com>
2025-09-16 19:54:46 +0800

    Added kelly page

---
+++ app/kelly/info.tsx
@@ -0,0 +1,6 @@
+export default function Info() {
+    return (
+        <h1 className="text-3xl font-bold">I like  nice people</h1>
+    );
+}
+
---
+++ app/kelly/page.tsx
@@ -0,0 +1,10 @@
+import Info from "./info";
+
+export default function Page() {
+    return (
+        <div>
+        <h1 className="text-3xl font-bold">James Kelly E. Espera</h1>
+        <Info />
+        </div>
+    );
+}
