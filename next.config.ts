701ddc6ed72973f6e2fbfc1468b84cfdd307f61f
James Kelly Espera <esperajameskelly123@gmail.com>
2025-11-20 13:12:41 +0800

    Updated .env file

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/dashboard/kelly',
+        destination: '/portfolio/index.html',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
